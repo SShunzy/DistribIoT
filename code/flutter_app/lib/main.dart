afc958d36e7167e22e0ea0824c9e68625892b7dc
Lionel Thys <lionel.thys@student.hepl.be>
2025-01-17 04:17:54 +0100

    FIX - Small Refactor of main function

--- code/flutter_app/lib/main.dart
+++ code/flutter_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/pages/map_page.dart';
+import 'package:flutter_app/pages/register_page.dart';
 
 void main() {
   runApp(const CupertinoApp(
     title: 'Flutter App',
-    home: CupertinoPageScaffold(
-        child: MapPage()
+    home:  MapPage()
     ),
-  ));
+  );
 }
