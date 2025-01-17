767bff685a92d0a66db068c416fdbee9729c0e36
Lionel Thys <lionel.thys@student.hepl.be>
2025-01-17 04:17:14 +0100

    FIX - Refactor of NavigationBar

--- code/flutter_app/lib/pages/products_page.dart
+++ code/flutter_app/lib/pages/products_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/review_page.dart';
 import 'package:flutter_app/widget/product_list.dart';
 import 'dart:convert' show utf8;
 
@@ -11,23 +12,56 @@ class ProductsPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar:  PreferredSize(
-          preferredSize:  const Size(180, 110),
-          child: SizedBox(
-              height: 110,
-              child:  CupertinoNavigationBar(
-                middle: Text(
-                    utf8.decode(machineName.codeUnits),
+    return  CupertinoPageScaffold(
+        child: CustomScrollView(
+          scrollBehavior: ScrollBehavior(),
+          slivers: [
+            CupertinoSliverNavigationBar(
+              alwaysShowMiddle: false,
+              leading: CupertinoButton(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child:  const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children:[Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.arrow_back_ios, size: 20),
+                      Text("Carte",style: TextStyle(fontSize: 20),)
+                    ],
+                  )
+                    ]
+                ),
+                onPressed: ()=> Navigator.pop(
+                  context,
+                ),
+              ),
+                largeTitle:Text(
+                  utf8.decode(machineName.codeUnits),
                   style: const TextStyle(height: 1, fontSize: 30),
                 ),
-                backgroundColor: CupertinoColors.black,
-              )
-          ),
-        ),
-        resizeToAvoidBottomInset: true,
-        body: Center(
-          child: ProductList(machineId: machineId),
+              trailing: CupertinoButton(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child:  const Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children:[Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Review",style: TextStyle(fontSize: 20),),
+                          Icon(Icons.star_half, size: 20),
+                        ],
+                      )
+                      ]
+                  ),
+                  onPressed: ()=> Navigator.push(
+                    context,
+                      CupertinoPageRoute(
+                      builder: (context) =>  ReviewPage(machineId: machineId, machineName: machineName)))
+                  ),
+            ),
+            SliverFillRemaining(
+              child: ProductList(machineId: machineId),
+            )
+          ],
         )
     );
   }
